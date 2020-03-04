9a81940dc1217dfffc3a78fcf15f349619d7d2b9
JaeSeoKim <48559454+JaeSeoKim@users.noreply.github.com>
2020-03-04 16:29:33 +0900

    #3.4 Add Navigation-bar

---
+++ src/routes/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./About.css";
+
+function About(){
+  return <div className="about__container">
+      <h2>The Movie App</h2>
+      <span>Created by jaeseokim</span>
+      <span>
+        “Freedom is the freedom to say that two plus two make four. If that is
+        granted, all else follows.”
+      </span>
+      <span>− George Orwell, 1984</span>
+    </div>
+}
+
+export default About;
